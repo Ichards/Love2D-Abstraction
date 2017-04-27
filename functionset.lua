f49d6618f6f90ab5d1b294b161b454c51ccbe152
Koopolt <keanurds@gmail.com>
2017-04-27 01:14:27 -0400

    Update functionset.lua

--- functionset.lua
+++ functionset.lua
@@ -1,12 +1,11 @@
 
 --Make this run in love.load, if possible.
+--That is NOT possible.
 
 local game = require("game")
 local menu = require("menu")
 local currentworld = game
-if currentworld.world = 1 then
-	currentworld = menu
-end
+
 
 local functions = {}
 functions.deltaTime = 0
@@ -16,12 +15,15 @@ functions.world = currentworld.world
 
 currentworld.deltaTime = functions.deltaTime
 
-function functions.load()
-	currentworld.load()
-end
+
 
 
 function functions.update()
+	
+	if currentworld.world == 1 then
+	currentworld = menu
+	end
+	
 	currentworld.deltaTime = functions.deltaTime
 	currentworld.update()
 end
@@ -29,7 +31,7 @@ end
 
 
 function functions.draw()
-	game.draw()
+	currentworld.draw()
 end
 
 
